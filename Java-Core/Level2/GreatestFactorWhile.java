6ffaec8edca3e2deba3befcb522f8a73f6b0315e
Chandra-Shekhar-24 <chandrashekharbansal.2006@gmail.com>
2025-08-19 12:45:06 +0530

    added: new files

---
+++ Java-Core/Level2/GreatestFactorWhile.java
@@ -0,0 +1,23 @@
+package Level2;
+
+import java.util.Scanner;
+
+public class GreatestFactorWhile {
+      public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number: ");
+        int num = sc.nextInt();
+
+        int greatestFactor = 1;
+        int counter = num - 1;
+
+        while (counter >= 1) {
+            if (num % counter == 0) {
+                greatestFactor = counter;
+                break;
+            }
+            counter--;
+        }
+        System.out.println("Greatest factor besides " + num + " is: " + greatestFactor);
+    }
+}
